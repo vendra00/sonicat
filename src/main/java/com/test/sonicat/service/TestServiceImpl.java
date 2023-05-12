f85bb94e71257b83d92ae005f887df6aacb84489
vendra00 <gabriel_vendramini@hotmail.com>
2023-05-12 18:10:57 +0200

    Some exceptions to the project

--- src/main/java/com/test/sonicat/service/TestServiceImpl.java
+++ src/main/java/com/test/sonicat/service/TestServiceImpl.java
@@ -34,31 +34,47 @@ public class TestServiceImpl implements TestService {
 
     @Override
     public Yeasts createYeastsTest(Yeasts yeasts) {
-        log.info("createYeastsTest called with yeasts: {}", yeasts);
-        yeasts.setIdentifier(testUtils.generateIdentifier("yeasts"));
-        yeasts.setYeastsConcentration(((float) yeasts.getNumberYeasts() / 100) * 0.000091f);
-        return testRepository.insert(yeasts);
+        try {
+            log.info("createYeastsTest called with yeasts: {}", yeasts);
+            yeasts.setIdentifier(testUtils.generateIdentifier("yeasts"));
+            yeasts.setYeastsConcentration(((float) yeasts.getNumberYeasts() / 100) * 0.000091f);
+            return testRepository.insert(yeasts);
+        } catch (Exception e) {
+            log.error("Error creating yeasts test: {}", e.getMessage());
+            throw new RuntimeException("Failed to create yeasts test");
+        }
     }
 
     @Override
     public Crystals createCrystalsTest(Crystals crystals) {
-        log.info("createCrystalsTest called with crystals: {}", crystals);
-        crystals.setIdentifier(testUtils.generateIdentifier("crystals"));
-        return testRepository.insert(crystals);
+        try {
+            log.info("createCrystalsTest called with crystals: {}", crystals);
+            crystals.setIdentifier(testUtils.generateIdentifier("crystals"));
+            return testRepository.insert(crystals);
+        } catch (Exception e) {
+            log.error("Error creating crystals test: {}", e.getMessage());
+            throw new RuntimeException("Failed to create crystals test");
+        }
     }
 
     @Override
     public Page<Test> getAllTests(Pageable pageable) {
-        log.info("getAllTests called with pageable: {}", pageable);
-        Page<Crystals> crystalsPage = crystalsRepository.findAll(pageable);
-        Page<Yeasts> yeastsPage = yeastsRepository.findAll(pageable);
+        try {
+            log.info("getAllTests called with pageable: {}", pageable);
+            Page<Crystals> crystalsPage = crystalsRepository.findAll(pageable);
+            Page<Yeasts> yeastsPage = yeastsRepository.findAll(pageable);
 
-        List<Test> testList = new ArrayList<>();
-        testList.addAll(crystalsPage.getContent());
-        testList.addAll(yeastsPage.getContent());
-        log.info("getAllTests returning {} tests", testList.size());
-        return new PageImpl<>(testList, pageable, crystalsPage.getTotalElements() + yeastsPage.getTotalElements());
+            List<Test> testList = new ArrayList<>();
+            testList.addAll(crystalsPage.getContent());
+            testList.addAll(yeastsPage.getContent());
+            log.info("getAllTests returning {} tests", testList.size());
+            return new PageImpl<>(testList, pageable, crystalsPage.getTotalElements() + yeastsPage.getTotalElements());
+        } catch (Exception e) {
+            log.error("Error retrieving all tests: {}", e.getMessage());
+            throw new RuntimeException("Failed to retrieve all tests");
+        }
     }
+
 
 }
 
